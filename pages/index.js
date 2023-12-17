4438b73e5b7c96338bfcc9321046b016045ab819
unimat <unimat@unimat.hu>
2023-12-17 14:53:16 +0100

    Getting Started With The Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
-function HomePage() {}
+import { Fragment } from "react/cjs/react.production.min"
+
+function HomePage() {
+  <Fragment>
+    <Hero />
+    <FeaturedPosts />
+  </Fragment>
+}
 
 export default HomePage
