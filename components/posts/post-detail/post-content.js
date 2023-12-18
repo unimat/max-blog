6d5f38dafa07f4eeaa90a8f4c2e4f168951c3f7b
unimat <unimat@unimat.hu>
2023-12-18 16:32:06 +0100

    Creating & Using Markdown Data For Rendering Posts

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,8 @@
+import ReactMarkdown from 'react-markdown';
+// import Image from 'next/image';
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
@@ -13,11 +18,57 @@ function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
-  
+
+  // const customRenderers = {
+  //   // img(image) {
+  //   //   return (
+  //   //     <Image
+  //   //       src={`/images/posts/${post.slug}/${image.src}`}
+  //   //       alt={image.alt}
+  //   //       width={600}
+  //   //       height={300}
+  //   //     />
+  //   //   );
+  //   // },
+  //   p(paragraph) {
+  //     const { node } = paragraph;
+
+  //     if (node.children[0].tagName === 'img') {
+  //       const image = node.children[0];
+
+  //       return (
+  //         <div className={classes.image}>
+  //           <Image
+  //             src={`/images/posts/${post.slug}/${image.properties.src}`}
+  //             alt={image.alt}
+  //             width={600}
+  //             height={300}
+  //           />
+  //         </div>
+  //       );
+  //     }
+
+  //     return <p>{paragraph.children}</p>;
+  //   },
+
+  //   code(code) {
+  //     const { className, children } = code;
+  //     const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+  //     return (
+  //       <SyntaxHighlighter
+  //         style={atomDark}
+  //         language={language}
+  //         children={children}
+  //       />
+  //     );
+  //   },
+  // };
+
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      {/* <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown> */}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,64 +1,24 @@
-// import Head from 'next/head';
 import { Fragment } from 'react';
 
 import AllPosts from '../../components/posts/all-posts';
-// import { getAllPosts } from '../../lib/posts-util';
-
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10'
-  }
-]
+import { getAllPosts } from '../../lib/posts-util';
 
 function AllPostsPage(props) {
   return (
     <Fragment>
-      {/* <Head>
-        <title>All Posts</title>
-        <meta
-          name='description'
-          content='A list of all programming-related tutorials and posts!'
-        />
-      </Head> */}
-      {/* <AllPosts posts={props.posts} /> */}
-      <AllPosts posts={DUMMY_POSTS} />
+      <AllPosts posts={props.posts} />
     </Fragment>
   );
 }
 
-// export function getStaticProps() {
-//   const allPosts = getAllPosts();
+export function getStaticProps() {
+  const allPosts = getAllPosts();
 
-//   return {
-//     props: {
-//       posts: allPosts,
-//     },
-//   };
-// }
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default AllPostsPage;
